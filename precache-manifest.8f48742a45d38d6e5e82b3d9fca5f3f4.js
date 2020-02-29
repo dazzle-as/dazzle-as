8893cd1407fec482c7e2d3e57ed294cc0f4c0e54
zaid <khanzaid786786.kz@gmail.com>
2020-02-29 12:57:47 +0530

    Updates

---
+++ precache-manifest.8f48742a45d38d6e5e82b3d9fca5f3f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d599a150b99f99c28239c0a96f31e8",
+    "url": "/dazzle-as/index.html"
+  },
+  {
+    "revision": "f7d18fbab517e195810d",
+    "url": "/dazzle-as/static/css/main.20ca0428.chunk.css"
+  },
+  {
+    "revision": "8944e87e71285e1bf690",
+    "url": "/dazzle-as/static/js/2.48c127f8.chunk.js"
+  },
+  {
+    "revision": "43e75541cb2c99a81f43877710dd8732",
+    "url": "/dazzle-as/static/js/2.48c127f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7d18fbab517e195810d",
+    "url": "/dazzle-as/static/js/main.cb6b6e45.chunk.js"
+  },
+  {
+    "revision": "adb16d4a8937e146e819",
+    "url": "/dazzle-as/static/js/runtime-main.fe2fc62c.js"
+  }
+]);
